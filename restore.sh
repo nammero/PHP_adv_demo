3dbdf7049e7c6ca0f030e8dee5a51d0b1b6a4c5f
nammero <zeugenep@gmail.com>
2019-02-20 15:39:36 +0200

    Fixed restore script

--- restore.sh
+++ restore.sh
@@ -2,18 +2,22 @@
 
 set -e
 
-if [ "$1" != "" ]; then
-	FOLDER_PATH=backups\/$1
+if [[ "$1" ]]; then
+    if [[ $1 =~ ^[0-9]{2}\-[0-9]{2}\-[0-9]{4}$ ]]; then
+        	FOLDER_PATH=backups\/$1
 #	echo "Folder path: $FOLDER_PATH"
+	else
+	echo "Wrong folder name format! Try 'dd-mm-yyyy'"
+	exit 1
+    fi
 else
 	echo "No folder name!"
-	exit 0
+	exit 1
 fi
 
-gzip -d ~/$FOLDER_PATH/db-dump.sql.gz
+gzip -dk ~/$FOLDER_PATH/db-dump.sql.gz
 mv ~/$FOLDER_PATH/db-dump.sql ~/
 
-unzip ~/$FOLDER_PATH/uploads.zip -d uploads
-mv ~/$FOLDER_PATH/uploads/* ~/backend/web/uploads
+unzip ~/$FOLDER_PATH/uploads.zip -d ~/backend/web/uploads
 
 echo "Restore finished!"
